c81b626b4ce85d6d9ab42d15e31a3d261f753001
Shumailz <shumailzafar561@gmail.com>
2026-02-19 10:06:03 +0500

    Create Gun.cpp

---
+++ Week 1/Gun.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main() {
+system("color 09");
+cout<<" +--^--------------, ----------, ------,------------^-,"<<endl;
+cout<<" |  |||||||||||      '--------'          |              O"<<endl;
+cout<<" '+--------------------------------------^--------------|"<<endl;
+cout<<" '-----------,  ----------, ------------------------' "<<endl;
+cout<<" '  /XXXXXX  / ' |         /' "<<endl;
+cout<<"   / XXXXXX /    |        /' "<<endl;
+cout<<"  / XXXXXX /'   ----------', "<<endl;
+cout<<" /  XXXXXX/"<<endl;
+cout<<"/ XXXXXXX/"<<endl;
+cout<<"(-------("<<endl;
+cout<<"'-------' "<<endl;
+}
