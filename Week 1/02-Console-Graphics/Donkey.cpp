a831740dead4bc6bda3a8e6abebb4394885b4e4d
Shumailz <shumailzafar561@gmail.com>
2026-02-19 09:52:16 +0500

    Console Graphics

---
+++ Week 1/02-Console-Graphics/Donkey.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main() {
+cout<<" ---------------------"<<endl;
+cout<<endl;
+cout<<"o    ^___^"<<endl;
+cout<<endl;
+cout<<" o   (oo)\\_______"<<endl;
+cout<<endl;
+cout<<"     (___)\\     )\\/\\"<<endl;
+cout<<endl;
+cout<<"          ||---W |"<<endl;
+cout<<endl;
+cout<<"          ||    ||"<<endl;
+}          
