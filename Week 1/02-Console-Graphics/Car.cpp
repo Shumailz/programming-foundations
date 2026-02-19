8ba69580a6a58de0323d4be78876e8695a8a2b74
Shumailz <shumailzafar561@gmail.com>
2026-02-19 10:49:46 +0500

    Cyberpunk  2077 style truck

---
+++ Week 1/02-Console-Graphics/Car.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main() {
+system("color 0D");
+cout<<"               _ _ _ .............._"<<endl;
+cout<<"            _ _..'_'.]]- .'-"<<endl;
+cout<<"         .-'    (_) |   ]]                ']] '_."<<endl;
+cout<<" _ _ _ _. "<<endl;
+cout<<"/_        ------------'-----]]---....._ _ _ ]]_ _'.' -.._ _ _"<<endl;
+cout<<"| T    _.-----._          Xxx|x....          |                '--. -"<<endl;
+cout<<"| |  .' ..--.. '.         xxx|xxxxxxxxxxXX== |        .'.---..'.     -._"<<endl;
+cout<<"]_j /  /  _ _ ) )         XXX|xxxxxxxxxxxx== |       / / _ _   ) )        '-."<<endl;
+cout<<" _l|  |  /   ) |  |       XXX|'             |      | | /  )   | |           |"<<endl;
+cout<<"l_ _)_|   (__/ |  | _ _ _ _ _|_ _ _ _ _ _ _  |_ _ _|   |  (_/   | | _ _ _ _ _ _ j"<<endl;
+cout<<"   '')(       /./_ _ _ ____ _ _ __ __ __ _ _ _ _ _ (   (       /  /_ _ _ _ _ _ )"<<endl;
+cout<<"      '.'_ _ _'.'  dp                                 '.'_ _ _'.'"<<endl;
+cout<<"        ''                                           '' "<<endl;
+}
